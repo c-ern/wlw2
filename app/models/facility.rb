93e15e9f79e2eaa10382a36fe5107a97f917ad61
Steve Lair <steve.lair@gmx.de>
2012-09-07 11:09:24 +0200

    Karten in Show fuer facilities hinzugefuegt, nahe Standorte auch angezeigt

--- app/models/facility.rb
+++ app/models/facility.rb
@@ -1,5 +1,7 @@
 class Facility < ActiveRecord::Base
   attr_accessible :address, :latitude, :longitude, :name, :companies_attributes
+  geocoded_by :address # hier sage ich, dass der geocode auf der Adresse basiert
+  after_validation :geocode, :if => :address_changed?
 
   has_many :affiliations
   has_many :companies, :through => :affiliations
